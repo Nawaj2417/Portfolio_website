619684601b10af59b1aefe85456f8a59b5a68ca8
Nawaj2417 <mdnawajn27@gmail.com>
2024-08-13 14:59:24 +0545

    changes

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -127,10 +127,11 @@ STATICFILES_DIRS = [
     
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # vercel statics settingss
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
